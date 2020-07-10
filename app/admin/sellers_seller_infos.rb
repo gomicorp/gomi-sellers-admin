2dabbe506f078bf1f0ada5256d5ff4b7108dc55c
Bran <40688044+june20516@users.noreply.github.com>
2020-07-10 18:21:23 +0900

    active admin 초기화, 필요한 resource 추가

---
+++ app/admin/sellers_seller_infos.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Sellers::SellerInfo do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :finished, :cart_id, :enc_id, :admin_memo, :channel_id, :country_id, :ordered_at
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:finished, :cart_id, :enc_id, :admin_memo, :channel_id, :country_id, :ordered_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   root 'hello#world' # example
   get 'sign_out', to: 'application#basic_auth_sign_out', as: :sign_out
 end
