e04e1c14331f2bf87b6484ffa37760a16cf58843
Bran <40688044+june20516@users.noreply.github.com>
2020-07-21 19:22:42 +0900

    dependency 수정

--- app/models/sellers/store_info.rb
+++ app/models/sellers/store_info.rb
@@ -2,6 +2,6 @@ module Sellers
   class StoreInfo < ApplicationRecord
     validates_uniqueness_of :url, case_sensitive: false
     belongs_to :seller_info, class_name: 'Sellers::SellerInfo', dependent: :destroy
-    has_many :selected_products, class_name: 'Sellers::SelectedProduct', dependent: :destroy
+    has_many :selected_products, class_name: 'Sellers::SelectedProduct', dependent: :nullify
   end
 end
